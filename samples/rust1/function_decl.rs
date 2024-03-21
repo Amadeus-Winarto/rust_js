1e47e3e4ebd2c41365597a71d8a87891096cec75
Amadeus Winarto <amadeusw6@gmail.com>
2024-03-21 14:39:33 +0800

    Feat: functions with non-empty unit type must have at least 1 return statement

--- samples/rust1/function_decl.rs
+++ samples/rust1/function_decl.rs
@@ -3,5 +3,5 @@ fn main() -> () {
 }
 
 fn add(a: i32, b: i32) -> i32 {
-    a + b; 
+    return a + b; 
 }
--- src/validators/syntax.ts
+++ src/validators/syntax.ts
@@ -2,6 +2,8 @@ import { AbstractParseTreeVisitor } from 'antlr4ts/tree/AbstractParseTreeVisitor
 import { Validator } from './types';
 import { print } from './utils';
 import { ErrorNode } from 'antlr4ts/tree/ErrorNode';
+import { Function_bodyContext, Function_declarationContext } from '../grammars/Rust1Parser';
+import { visitFunctionBody } from 'typescript';
 
 /**
  * Implements the rule: all variables must be declared at most once before they are used.
@@ -24,9 +26,35 @@ export class SyntaxValidator extends AbstractParseTreeVisitor<boolean> implement
         return aggregate && nextResult;
     }
 
+    visitFunction_declaration(ctx: Function_declarationContext): boolean {
+        // If function has a non-empty return type, it must have a return statement
+        // If function has a empty return type, it must not have a return statement
+
+        const return_type_string = ctx.type().text;
+        console.log(return_type_string);
+
+        const has_empty_return_type = return_type_string === "()";
+        const body = ctx.function_body();
+
+        return this.have_return_statement(body, !has_empty_return_type);
+    }
 
     visitErrorNode(node: ErrorNode): boolean {
         this.print_fn("Visiting error node");
         return false;
+    }
+
+    have_return_statement(body: Function_bodyContext, expect_return_statement: boolean): boolean {
+        const return_statements = body.block().statement().filter(statement => statement.return_expression() !== undefined);
+
+        if (expect_return_statement && return_statements.length === 0) {
+            this.print_fn("No return statement found but expecting one");
+            return false;
+        } else if (!expect_return_statement && return_statements.length > 0) {
+            this.print_fn("Return statement found but not expecting one");
+            return false;
+        }
+
+        return true;
     }
 }
